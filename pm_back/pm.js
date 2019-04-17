038d395a628fe98a84f1a6a3090fede782477b24
Christopher Siewert <christopher.siewert@live.ca>
2019-04-17 12:27:29 -0700

    Comments on backend files

--- pm_back/pm.js
+++ pm_back/pm.js
@@ -1,6 +1,7 @@
 
 /* global getAssetRegistry getFactory emit */
 
+// Cost function according to LMSR
 function costCalc(q1, q2) {
   	const b = 100
     return b * Math.log(Math.exp(q1/b) + Math.exp(q2/b))
@@ -12,66 +13,75 @@ function costCalc(q1, q2) {
  * @param {org.example.basic.BuyShares} tx The sample transaction instance.
  * @transaction
  */
-async function buyShares(tx) {  // eslint-disable-line no-unused-vars
-  
+async function buyShares(tx) {
+
+    // Create new factory, which is used to create new assets
   	var factory = getFactory()
-	var NS = "org.example.basic"
+	  var NS = "org.example.basic"
 
   	let trader = tx.trader
     let market = tx.market
-  
+
+    // Shouldn't be able to trade if the market is over
     if (market.isResolved == true) {
     	throw new Error("Market is resolved")
     }
   	let q1 = market.yesShares
-    let q2 = market.noShares 
-    
+    let q2 = market.noShares
+
+    // Cost differs if you are buying yes or no tokens
     let cost
     if (tx.type == "YES") {
+      // According to LMSR you take difference in cost functions when you add
+      // new shares
     	cost = costCalc(q1 + tx.amount, q2) - costCalc(q1, q2)
     } else {
     	cost = costCalc(q1, q2 + tx.amount) - costCalc(q1, q2)
     }
-    
+
+    // Prevent against overspending
     if (trader.balance < cost) {
     	throw new Error("Balance too low")
     }
-    
+
+    // Deduct balance
   	trader.balance -= cost
-  	
-  	// Search for market in trader array of shares
+
+  	// Search for this particular market in trader array of shares
   	var index = -1;
-  
+
   	for (let i = 0; i < trader.shares.length; i++) {
     	if (trader.shares[i].market.marketId == market.marketId) {
           	index = i
             break
         }
     }
-  	
-  	// If not found, push and set index to last
+
+  	// If not found, push new object and set index to last
   	if (index == -1) {
+        // Factory object is a composer object to create new objects
       	var share = factory.newConcept(NS, 'Shares')
         share.market = factory.newRelationship(NS, 'Market', market.marketId)
       	share.yesShares = 0
       	share.noShares = 0
-    	trader.shares.push(share)
+    	  trader.shares.push(share)
       	index = trader.shares.length - 1
     }
     // Now index is index of the proper market
+    // Add amount of shares purchased to market total, and to trader array
   	if (tx.type == "YES") {
-    	market.yesShares += tx.amount
+    	  market.yesShares += tx.amount
       	trader.shares[index].yesShares += tx.amount
     } else {
-    	market.noShares += tx.amount
+    	  market.noShares += tx.amount
       	trader.shares[index].noShares += tx.amount
     }
-  
+
     // Get the participant registry.
     const participantRegistry = await getParticipantRegistry('org.example.basic.Trader');
     // Update the asset in the asset registry.
     await participantRegistry.update(trader);
-  
+
     // Get the asset registry.
     const assetRegistry = await getAssetRegistry('org.example.basic.Market');
     // Update the asset in the asset registry.
@@ -84,59 +94,67 @@ async function buyShares(tx) {  // eslint-disable-line no-unused-vars
  * @param {org.example.basic.SellShares} tx The sample transaction instance.
  * @transaction
  */
-async function sellShares(tx) {  // eslint-disable-line no-unused-vars
-  
+async function sellShares(tx) {
+
+    // Composer factory object
   	var factory = getFactory()
-	var NS = "org.example.basic"
+	  var NS = "org.example.basic"
 
   	let trader = tx.trader
     let market = tx.market
-  	
-	if (market.isResolved == true) {
+
+    // Can't sell shares if market is done
+	  if (market.isResolved == true) {
     	throw new Error("Market is resolved")
     }
-  
+
     // Search for existing market
     var index = -1;
-  
+
+    // Loop through looking for matching ID
   	for (let i = 0; i < trader.shares.length; i++) {
     	if (trader.shares[i].market.marketId == market.marketId) {
           	index = i
             break
         }
     }
-  
-  	// Now index is index of correct market in trader array
+
+    // If index doesn't get updated, marekt doesn't exist in array
   	if (index == -1) {
     	throw new Error("No existing shares")
     }
-  
+    // Now index is index of correct market in trader array
   	let q1 = tx.market.yesShares
-    let q2 = tx.market.noShares 
-    
+    let q2 = tx.market.noShares
+
+    // If selling yes shares
   	if (tx.type == "YES") {
-    	if (trader.shares[index].yesShares < tx.amount) {
+        // If you aren't trying to sell too many
+    	  if (trader.shares[index].yesShares < tx.amount) {
         	throw new Error("Not enough shares")
         }
+        // Decrement trader shares and market total of shares
       	trader.shares[index].yesShares -= tx.amount
       	market.yesShares -= tx.amount
-      
+        // Increase trader balance by the amount according to LMSR
       	trader.balance -= costCalc(q1 - tx.amount, q2) - costCalc(q1, q2)
-    } else {
-    	if (trader.shares[index].noShares < tx.amount) {
+    } else { // If selling no shares
+        // If you aren't trying to sell too many
+    	  if (trader.shares[index].noShares < tx.amount) {
         	throw new Error("Not enough shares")
         }
+        // Decrement no shares of trader and market total in circulation
       	trader.shares[index].noShares -= tx.amount
       	market.noShares -= tx.amount
-      
+        // Increase trader balance by the amount according to LMSR
       	trader.balance -= costCalc(q1, q2 - tx.amount) - costCalc(q1, q2)
     }
-  	
+
   	    // Get the participant registry.
     const participantRegistry = await getParticipantRegistry('org.example.basic.Trader');
     // Update the asset in the asset registry.
     await participantRegistry.update(trader);
-  
+
     // Get the asset registry.
     const assetRegistry = await getAssetRegistry('org.example.basic.Market');
     // Update the asset in the asset registry.
@@ -150,16 +168,16 @@ async function sellShares(tx) {  // eslint-disable-line no-unused-vars
  */
 
 async function resolveMarket(tx) {
-	
+    // Provide an answer to a market and stop trading
   	let market = tx.market
     market.answer = tx.answer
   	market.isResolved = true
-  
+
      // Get the asset registry.
     const assetRegistry = await getAssetRegistry('org.example.basic.Market');
     // Update the asset in the asset registry.
     await assetRegistry.update(market);
-  
+
 }
 
 /**
@@ -169,52 +187,54 @@ async function resolveMarket(tx) {
  */
 
 async function claimProfits(tx) {
-	
+    // Burns all tokens of trader, gives back $
   	let market = tx.market
     let trader = tx.trader
-  
+
     if (market.isResolved == false) {
     	throw new Error("Market is not yet resolved")
     }
-  
+
     // Search for existing market
     var index = -1;
-  
+
   	for (let i = 0; i < trader.shares.length; i++) {
     	if (trader.shares[i].market.marketId == market.marketId) {
           	index = i
             break
         }
     }
-  
-  	// Now index is index of correct market in trader array
+    // Now index is either -1 if no market exists, or is correct index
+    // in the trader array of markets
   	if (index == -1) {
     	throw new Error("No existing shares")
     }
-  	
+
+    // Payment depends on yes or no tokens
   	let payment
-    
   	if (market.answer == "YES") {
-    	payment = trader.shares[index].yesShares   	
+    	payment = trader.shares[index].yesShares
     } else {
     	payment = trader.shares[index].noShares
     }
-  
+
+    // Decrease total in circulation
   	market.yesShares -= trader.shares[index].yesShares
   	market.noShares -= trader.shares[index].noShares
+    // Set trader shares to 0
   	trader.shares[index].yesShares = 0
   	trader.shares[index].noShares = 0
-
+    // Pay trader $1 for each correct token
     trader.balance += payment
-  	
+
  	// Get the participant registry.
     const participantRegistry = await getParticipantRegistry('org.example.basic.Trader');
     // Update the asset in the asset registry.
     await participantRegistry.update(trader);
-  
+
     // Get the asset registry.
     const assetRegistry = await getAssetRegistry('org.example.basic.Market');
     // Update the asset in the asset registry.
     await assetRegistry.update(market);
-  
+
 }
